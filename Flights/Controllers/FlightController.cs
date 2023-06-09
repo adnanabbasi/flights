f0a390ffe8aec372810c0c1a728f1de4b318ea0c
Adnan Abbasi <adnan_abbasi@yahoo.com>
2023-06-09 15:51:47 -0400

    Day 7: Parameterizing Angular Routes

--- Flights/Controllers/FlightController.cs
+++ Flights/Controllers/FlightController.cs
@@ -10,18 +10,11 @@ namespace Flights.Controllers
     {
         private readonly ILogger<FlightController> _logger;
 
-        public FlightController(ILogger<FlightController> logger)
+        static Random random = new Random();
+
+        private static FlightRm[] flights = new FlightRm[] 
         {
-            _logger = logger;
-        }
-
-        Random random = new Random();
-
-        [HttpGet]
-        public IEnumerable<FlightRm> Search()
-            => new FlightRm[]
-            {
-                new (   Guid.NewGuid(),
+            new (   Guid.NewGuid(),
                 "American Airlines",
                 random.Next(90, 5000).ToString(),
                 new TimePlaceRm("Los Angeles",DateTime.Now.AddHours(random.Next(1, 3))),
@@ -69,7 +62,20 @@ namespace Flights.Controllers
                 new TimePlaceRm("Le Bourget",DateTime.Now.AddHours(random.Next(1, 58))),
                 new TimePlaceRm("Zagreb",DateTime.Now.AddHours(random.Next(4, 60))),
                     random.Next(1, 853))
-            };
+        };
+
+        public FlightController(ILogger<FlightController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<FlightRm> Search()
+        => flights;
+
+        [HttpGet("{id}")]
+        public FlightRm Find(Guid id)
+        => flights.SingleOrDefault(f => f.Id == id); 
 
     }
 }
