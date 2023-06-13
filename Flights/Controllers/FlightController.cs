de0b823f9146fed0cb5af82b08cd086068f2675f
Adnan Abbasi <adnan_abbasi@yahoo.com>
2023-06-13 19:04:34 -0400

    Day 8: Documenting HTTP Response Codes

--- Flights/Controllers/FlightController.cs
+++ Flights/Controllers/FlightController.cs
@@ -69,13 +69,26 @@ namespace Flights.Controllers
             _logger = logger;
         }
 
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(typeof(IEnumerable<FlightRm>), 200)]
         [HttpGet]
         public IEnumerable<FlightRm> Search()
         => flights;
 
+        [ProducesResponseType(400)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(typeof(FlightRm), 200)]
         [HttpGet("{id}")]
-        public FlightRm Find(Guid id)
-        => flights.SingleOrDefault(f => f.Id == id); 
+        public ActionResult<FlightRm> Find(Guid id)
+        {
+            var flight = flights.SingleOrDefault(f => f.Id == id);
+
+            if (flight == null)
+                return NotFound();
+            
+            return Ok(flight);
+        } 
 
     }
 }
